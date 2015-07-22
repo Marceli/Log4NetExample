a6b9d61053ef14bece85bfae3e4a2d4ac1e74b6b
Marceli <marcel.piekalkiewicz@gmail.com>
2015-07-22 19:47:57 +0200

    First Check in.

---
+++ Log4NetExample/MyCustomAppender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Core;
+using log4net.Appender;
+
+namespace Log4NetExample
+{
+    public class MyCustomAppender : AppenderSkeleton
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            string message = RenderLoggingEvent(loggingEvent);
+            Console.WriteLine("From Custom Appender" + message);
+        }
+    }
+}
---
+++ Log4NetExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Log4NetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log=LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            var i = 0;
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    break;
+                }
+                log.Info(i);
+               
+                i++;
+                Thread.Sleep(1000);
+
+
+            }
+        }
+    }
+}
